4216a97b4c2cf53df240bd501771a332ae15a792
Nastrx <mghyfari@gmail.com>
2025-12-02 17:46:37 +0700

    m

--- TelUD/app/Models/order_details.php
+++ TelUD/app/Models/order_details.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class OrderDetail extends Model
 {
     protected $table = 'order_details';
--- TelUD/database/migrations/2025_11_23_093618_kategori.php
+++ TelUD/database/migrations/2025_11_23_093618_kategori.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+    
 
     public function down()
     {
