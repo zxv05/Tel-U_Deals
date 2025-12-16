75ef813662a996d9841d2c0f59a243e4508cb4f7
RabihP <rabihperkasa04@gmail.com>
2025-12-16 17:11:44 +0700

    update2

--- TelUD/app/Models/product.php
+++ TelUD/app/Models/product.php
@@ -2,25 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
 
-    protected $primaryKey = 'ProductID';
+    protected $table = 'product'; // Nama tabel singular
+    protected $primaryKey = 'ProductID'; // PK Custom
+    
+    // Biar bisa mass assignment
+    protected $fillable = [
+        'fk_user', 'NamaBarang', 'fk_kategori', 'HargaProduct', 'ProductDetail'
+    ];
 
-    public function user()
+    // Relasi ke User
+    public function seller()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'fk_user', 'IdUser');
     }
 
+    // Relasi ke Kategori
     public function kategori()
     {
-        return $this->belongsTo(Kategori::class);
-    }
-
-    public function orderDetails()
-    {
-        return $this->hasMany(OrderDetail::class);
+        return $this->belongsTo(Kategori::class, 'fk_kategori', 'KategoriID');
     }
 }
