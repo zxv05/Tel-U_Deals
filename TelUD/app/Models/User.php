8255688d45f42e67bd3ec6c3b3ebb0367a5527e6
RabihP <rabihperkasa04@gmail.com>
2025-12-16 17:15:07 +0700

    merging fix

--- TelUD/app/Models/User.php
+++ TelUD/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
+Use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
